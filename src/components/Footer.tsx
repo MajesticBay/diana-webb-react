4b7c12f1282d75a40648dfd2f6fd405bc73276a4
Sergey Sherstobitov <sergey.sherstobitov@sas.com>
2024-05-04 02:26:32 -0400

    Minor changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => (
     <div className="footer-mobile">
       <h3 className="footer-mobile__header">Diana Webb</h3>
       <p className="footer-mobile__text">
-        © 2021 Diana Webb. All Rights Reserved.{' '}
+        © 2024 Diana Webb. All Rights Reserved.{' '}
       </p>{' '}
       {/* <p className="footer-mobile__text">
         <a className="footer-mobile__link" href="http://23rdsquad.us/">
@@ -15,7 +15,7 @@ const Footer = () => (
     <div className="footer-desktop">
       <h3 className="footer-desktop__header">Diana Webb</h3>
       <p className="footer-desktop__text">
-        © 2021 Diana Webb. All Rights Reserved.{' '}
+        © 2024 Diana Webb. All Rights Reserved.{' '}
         {/* <a className="footer-desktop__link" href="http://23rdsquad.us/">
           Website by 23rd Squad
         </a> */}
